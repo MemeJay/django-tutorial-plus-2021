6962a90f96c6167b37d8da8bf41b137c54cd212b
MemeJay <94447106+MemeJay@users.noreply.github.com>
2022-01-29 12:54:49 +0800

    Django tutorial beg sect 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,18 @@
 from http.client import HTTPResponse
+from tkinter.messagebox import QUESTION
 from django.shortcuts import HttpResponse
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_ID):
+    return HttpResponse("You're looking at question %s." % question_ID)
+
+def results(request, question_ID):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_ID)
+
+def vote(request, question_ID):
+    return HttpResponse("You're voting on question %s.")
